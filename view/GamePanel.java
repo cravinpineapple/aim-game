841b38be369386308846906e2bc2729c24953858
Noah Stinson <noahstjohn@ymail.com>
2020-09-14 11:54:44 -0500

    initial commit

---
+++ view/GamePanel.java
@@ -0,0 +1,21 @@
+package view;
+
+import javax.swing.JFrame;
+
+import model.Aim;
+
+public class GamePanel {
+	
+	private JFrame window;
+	private Aim aimModel;
+
+	public GamePanel(JFrame window, Aim aimModel) {
+		this.window = window;
+		this.aimModel = aimModel;
+	}
+
+	public void init() {
+		
+	}
+
+}
