6f520485b6d73b43a482e024db4a806fd67c7bc0
Noah Stinson <noahstjohn@ymail.com>
2020-09-15 13:53:49 -0500

    added listener for canvas

--- view/GamePanel.java
+++ view/GamePanel.java
@@ -5,12 +5,14 @@ import java.awt.Container;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import controller.GameEventListener;
 import model.Aim;
 
 public class GamePanel {
 	
 	private JFrame window;
 	private Aim aimModel;
+	GameCanvas canvas;
 
 	public GamePanel(JFrame window, Aim aimModel) {
 		this.window = window;
@@ -22,11 +24,20 @@ public class GamePanel {
 
 		// creates panel and adds in order: canvas->panel->cp (container)
 		JPanel panel = new JPanel();
-		GameCanvas canvas = new GameCanvas(this, aimModel);
+		canvas = new GameCanvas(this, aimModel);
 		panel.add(canvas);
 		cp.add(panel);
 
+		GameEventListener gameEventListener = new GameEventListener(this, aimModel);
+		canvas.addMouseListener(gameEventListener);
+	}
 
+	public JFrame getWindow() {
+		return window;
+	}
+
+	public GameCanvas getCanvas() {
+		return canvas;
 	}
 
 }
