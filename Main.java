bb92b702256ee673abe1c6994d184c19a6102548
Noah Stinson <noahstjohn@ymail.com>
2020-09-15 12:30:18 -0500

    screen size set to max

--- Main.java
+++ Main.java
@@ -6,14 +6,13 @@ public class Main {
 	public static void main(String[] args) {
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setLocation(200, 400);
-		window.setSize(1200, 1200);
 
 		var menu = new MenuScreen(window);
 		menu.init();
 
 		window.pack();
 		window.revalidate();
-		window.setVisible(true);;
+		window.setVisible(true);
+		window.setExtendedState(window.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 	}
 }
